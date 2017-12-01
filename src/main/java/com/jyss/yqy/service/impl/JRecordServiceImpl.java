b3f2d52bc84b6edf2b24f972842a059316b4485b
sosahh <1447961434@qq.com>
2017-12-01 19:11:56 +0800

    1

--- src/main/java/com/jyss/yqy/service/impl/JRecordServiceImpl.java
+++ src/main/java/com/jyss/yqy/service/impl/JRecordServiceImpl.java
@@ -1,13 +1,34 @@
 package com.jyss.yqy.service.impl;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.jyss.yqy.entity.JRecord;
+import com.jyss.yqy.mapper.JRecordMapper;
 import com.jyss.yqy.service.JRecordService;
 
 @Service
 @Transactional
 public class JRecordServiceImpl implements JRecordService{
+	
+	@Autowired
+	private JRecordMapper recordMapper;
+	
+	public Map<String,String> insertJBonusScj(){
+		Map<String,String> map = new HashMap<String,String>();
+		
+		List<JRecord> recordList = recordMapper.selectJRecord();
+		for (JRecord jRecord : recordList) {
+			
+		}
+		return map;
+		
+	}
 	
 	
 
